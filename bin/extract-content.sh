17afa91e7afbbc63d6c68cf3be7fc3c5045f5c9b
George Moyano <g@gmoyano.com>
2017-10-03 07:11:51 -0700

    * run documentation * bash script argument update * documentation array fix

--- bin/extract-content.sh
+++ bin/extract-content.sh
@@ -1,12 +1,28 @@
 #!/usr/bin/bash
 # execute from project base
-#SOURCE_XML_BASE="/home/g/g-dev/soi-messaging/tsoa-track/src/main/resources/iep-schema/"
+#XML_SCHEMA_CONST=""
+#CONTENT_OUTPUT_DIR_CONST=""
+XML_TRANSFORMER="/opt/saxonica/SaxonEE9-7-0-18J/saxon9ee.jar"
 if [ ${#} -eq 1 ] ; then
-  if [ -f ${SOURCE_XML_BASE}${1} ] ; then
-    java -jar /opt/saxonica/SaxonEE9-7-0-18J/saxon9ee.jar -xsl:./lib/extract-xml-schema-documentation.sef -s:${SOURCE_XML_BASE}${1}
+  XML_SCHEMA_ARG=${1}
+  if [ -n "${XML_SCHEMA_ARG}" ]; then
+    XML_SCHEMA=${XML_SCHEMA_ARG};
   else
-    echo "  file does not exist: ${SOURCE_XML_BASE}${1}" 
+    XML_SCHEMA=${XML_SCHEMA_CONST};
+  fi
+  # output is directed to console and may be directed to a file
+  #CONTENT_OUTPUT_DIR_ARG=${2}
+  #if [ -n "${CONTENT_OUTPUT_DIR_ARG}" ]; then
+  #  CONTENT_OUTPUT_DIR=${CONTENT_OUTPUT_DIR_ARG};
+  #else
+  #  CONTENT_OUTPUT_DIR=${CONTENT_OUTPUT_DIR_CONST};
+  #fi
+  #echo "CONTENT_OUTPUT_DIR: ${CONTENT_OUTPUT_DIR}"
+  if [ -f ${XML_SCHEMA} ] ; then
+    java -jar ${XML_TRANSFORMER} -xsl:./lib/extract-xml-schema-documentation.sef -s:${XML_SCHEMA}
+  else
+    echo -e "\n  file does not exist: ${XML_SCHEMA}\n" 
   fi
 else
-  echo -e "  usage: ${0} xml-schema-file"
+  echo -e "\n  usage: ${0} xml-schema-file\n"
 fi
